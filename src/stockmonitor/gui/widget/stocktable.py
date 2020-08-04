aa144022b4aa63fd00a60f87b2dacd8b8c23d399
anetczuk <anetczuk@o2.pl>
2020-08-05 00:47:24 +0200

    Display pandas data inside table

--- src/stockmonitor/gui/widget/stocktable.py
+++ src/stockmonitor/gui/widget/stocktable.py
@@ -23,222 +23,46 @@
 
 import logging
 
-from typing import List
-from datetime import datetime, date, timedelta
-
-from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QAbstractItemView, QHeaderView
 from PyQt5.QtCore import Qt
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtGui import QColor, QBrush
+# from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import QAbstractTableModel
+from PyQt5.QtWidgets import QTableView
+
+from pandas import DataFrame
 
 
 _LOGGER = logging.getLogger(__name__)
 
 
-class StockTable( QTableWidget ):
+class PandasModel( QAbstractTableModel ):
 
-#     selectedTask    = pyqtSignal( Task )
-#     taskUnselected  = pyqtSignal()
-#     editTask        = pyqtSignal( Task )
+    def __init__(self, data: DataFrame):
+        super().__init__()
+        self._data: DataFrame = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parnet=None):
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            colName = self._data.columns[section]
+            return colName[0]
+        return None
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+
+class StockTable( QTableView ):
 
     def __init__(self, parentWidget=None):
         super().__init__(parentWidget)
 
-        self.data = None
-        self.showCompleted = False
-
-        self.setSelectionBehavior( QAbstractItemView.SelectRows )
-        self.setSelectionMode( QAbstractItemView.SingleSelection )
-        self.setEditTriggers( QAbstractItemView.NoEditTriggers )
-
-#         self.setAlternatingRowColors( True )
-#         self.setStyleSheet("""
-#         QTableView::item:alternate {
-#             background-color: #bfffbf;
-#         }
-#         QTableView::item {
-#             background-color: #deffde;
-#         }
-#         """);
-
-        self.setShowGrid( False )
-
-        headerLabels = [ "Summary", "Priority", "Complete", "Start Date", "Due Date" ]
-        self.setColumnCount( len(headerLabels) )
-        self.setHorizontalHeaderLabels( headerLabels )
-
-        header = self.horizontalHeader()
-        header.setHighlightSections( False )
-        header.setSectionResizeMode( 0, QHeaderView.Stretch )
-
-#         self.taskContextMenu = TaskContextMenu( self )
-
-        self.itemSelectionChanged.connect( self.taskSelectionChanged )
-        self.itemClicked.connect( self.taskClicked )
-        self.itemDoubleClicked.connect( self.taskDoubleClicked )
-
-#         self.setTasks( [] )
-
-    def connectData(self, dataObject):
-        self.data = dataObject
-        self.taskContextMenu.connectData( dataObject )
-        self.editTask.connect( dataObject.editTask )
-
-    def clear(self):
-        self.setRowCount( 0 )
-        self.emitSelectedTask()
-
-    def showCompletedTasks(self, show):
-        self.showCompleted = show
-        self.updateView()
-
-    def updateView(self):
-        tasksList = self.data.getManager().getTasks()
-        self.setTasks( tasksList )
-
-#     def getTask(self, taskIndex) -> TaskOccurrence:
-#         if taskIndex < 0:
-#             return None
-#         if taskIndex >= self.rowCount():
-#             return None
-#         tableItem = self.item( taskIndex, 0 )
-#         userData: TaskOccurrence = tableItem.data( Qt.UserRole )
-#         return userData
-# 
-#     def setTasks( self, tasksList: List[Task] ):
-#         self.clear()
-# 
-#         self.setSortingEnabled( False )     ## workaround to fix disappearing cells content
-# 
-#         occurrencesList = [ task.currentOccurrence() for task in tasksList ]
-#         if self.showCompleted is False:
-#             occurrencesList = [ task for task in occurrencesList if not task.isCompleted() ]
-# 
-#         occurSize = len( occurrencesList )
-#         self.setRowCount( occurSize )
-# 
-#         nowDate = date.today()
-# 
-#         for i in range(0, occurSize):
-#             task: TaskOccurrence = occurrencesList[i]
-# 
-#             fgColor = get_task_fgcolor( task )
-#             bgColor = None
-#             if task.isInMonth( nowDate ):
-#                 bgColor = QColor( "beige" )
-# #                 bgColor = QColor( "#deffde" )
-# #                 bgColor = QColor( "#bfffbf" )
-# 
-#             titleItem = QTableWidgetItem( task.title )
-#             titleItem.setData( Qt.UserRole, task )
-#             titleItem.setForeground( fgColor )
-#             if bgColor is not None:
-#                 titleItem.setBackground( bgColor )
-#             self.setItem( i, 0, titleItem )
-# 
-#             priorityItem = QTableWidgetItem()
-#             priorityItem.setData( Qt.EditRole, task.priority )                      ## handles int type properly
-#             priorityItem.setTextAlignment( Qt.AlignHCenter | Qt.AlignVCenter )
-#             priorityItem.setForeground( fgColor )
-#             if bgColor is not None:
-#                 priorityItem.setBackground( bgColor )
-#             self.setItem( i, 1, priorityItem )
-# 
-#             completedItem = QTableWidgetItem()
-#             completedItem.setData( Qt.EditRole, task.completed )                    ## handles int type properly
-#             completedItem.setTextAlignment( Qt.AlignHCenter | Qt.AlignVCenter )
-#             completedItem.setForeground( fgColor )
-#             if bgColor is not None:
-#                 completedItem.setBackground( bgColor )
-#             self.setItem( i, 2, completedItem )
-# 
-#             startDate = "---"
-#             if task.start is not None:
-#                 ## no start date -- deadline case
-#                 startDate = task.start.strftime( "%Y-%m-%d %H:%M" )
-#             startItem = QTableWidgetItem( str(startDate) )
-#             startItem.setTextAlignment( Qt.AlignHCenter | Qt.AlignVCenter )
-#             startItem.setForeground( fgColor )
-#             if bgColor is not None:
-#                 startItem.setBackground( bgColor )
-#             self.setItem( i, 3, startItem )
-# 
-#             dueDate = "---"
-#             if task.due is not None:
-#                 dueDate = task.due.strftime( "%Y-%m-%d %H:%M" )
-#             #dueDate = task.occurrenceDue.date()
-#             dueItem = QTableWidgetItem( str(dueDate) )
-#             dueItem.setTextAlignment( Qt.AlignHCenter | Qt.AlignVCenter )
-#             dueItem.setForeground( fgColor )
-#             if bgColor is not None:
-#                 dueItem.setBackground( bgColor )
-#             self.setItem( i, 4, dueItem )
-# 
-#         self.setSortingEnabled( True )
-#         self.update()
-
-    def contextMenuEvent( self, event ):
-        evPos = event.pos()
-        taskOccurrence: TaskOccurrence = None
-        item = self.itemAt( evPos )
-        if item is not None:
-            rowIndex = self.row( item )
-            taskOccurrence: TaskOccurrence = self.getTask( rowIndex )
-        if taskOccurrence is not None:
-            self.taskContextMenu.show( taskOccurrence.task )
-        else:
-            self.taskContextMenu.show( None )
-
-    def taskSelectionChanged(self):
-        taskIndex = self.currentRow()
-        task: TaskOccurrence = self.getTask( taskIndex )
-        self.emitSelectedTask( task )
-
-    def taskClicked(self, item):
-        taskIndex = self.row( item )
-        task: TaskOccurrence = self.getTask( taskIndex )
-        self.emitSelectedTask( task )
-
-    def taskDoubleClicked(self, item):
-        rowIndex = self.row( item )
-        task: TaskOccurrence = self.getTask( rowIndex )
-        self.editTask.emit( task.task )
-
-#     def emitSelectedTask( self, task: TaskOccurrence=None ):
-#         if task is not None:
-#             task = task.task
-#             self.selectedTask.emit( task )
-#         else:
-#             self.taskUnselected.emit()
-
-
-# def get_task_fgcolor( task: TaskOccurrence ) -> QBrush:
-#     if task.isCompleted():
-#         ## completed -- green
-#         return QBrush( get_completed_color() )
-#     if task.isTimedout():
-#         ## timed out
-#         return QBrush( get_timeout_color() )
-#     if task.isReminded():
-#         ## already reminded
-#         return QBrush( get_reminded_color() )
-#     taskFirstDate = task.getFirstDateTime()
-#     if taskFirstDate is not None:
-#         diff = taskFirstDate - datetime.today()
-#         if diff > timedelta( days=90 ):
-#             ## far task -- light gray
-#             return QBrush( QColor( 180, 180, 180 ) )
-#     ## normal
-#     return QBrush( QColor(0, 0, 0) )
-
-
-def get_completed_color() -> QColor:
-    return QColor(0, 160, 0)        ## green
-
-
-def get_timeout_color() -> QColor:
-    return QColor(255, 0, 0)        ## red
-
-
-def get_reminded_color() -> QColor:
-    return QColor(200, 0, 200)      ## magenta
+    def setData(self, rawData: DataFrame ):
+        self.setModel( PandasModel( rawData ) )
