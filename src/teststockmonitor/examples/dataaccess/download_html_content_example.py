342c341f2941b5fcd3737de684190602ebcce039
anetczuk <anetczuk@o2.pl>
2022-03-04 22:17:16 +0100

    Add example

---
+++ src/teststockmonitor/examples/dataaccess/download_html_content_example.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+##
+##
+##
+
+try:
+    ## following import success only when file is directly executed from command line
+    ## otherwise will throw exception when executing as parameter for "python -m"
+    # pylint: disable=W0611
+    import __init__
+except ImportError:
+    ## when import fails then it means that the script was executed indirectly
+    ## in this case __init__ is already loaded
+    pass
+
+from stockmonitor.dataaccess import download_html_content
+
+
+if __name__ == '__main__':
+    
+    ##
+    ## get information about request
+    ##
+
+    url = "https://httpbin.org/anything"
+    content = download_html_content( url, None )
+    response = content.decode()
+    print( response )
