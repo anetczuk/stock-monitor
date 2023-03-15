a7c0d4e9d6ee01b89257bc1f6897237b81908001
anetczuk <anetczuk@o2.pl>
2023-03-15 23:22:23 +0100

    Update scripts

--- tools/installvenv.sh
+++ tools/installvenv.sh
@@ -126,7 +126,7 @@ set -eu
 
 
 ### creating project start script
-create_venv_shortcut "$VENV_DIR/activatevenv.sh \"$SRC_DIR/startmonitor; exit\"" "$VENV_DIR/startmonitor"
+create_venv_shortcut "$VENV_DIR/activatevenv.sh \"$SRC_DIR/startmonitor \$@; exit\"" "$VENV_DIR/startmonitor"
 
 ### creating menu configuration script
 create_venv_shortcut "$SRC_DIR/configure_menu.sh $VENV_DIR/activatevenv.sh" "$VENV_DIR/configure_menu.sh"
