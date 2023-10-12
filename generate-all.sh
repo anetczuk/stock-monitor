382609e17bf8da017351998e1826a90f392c36ff
anetczuk <anetczuk@o2.pl>
2023-10-12 21:32:19 +0200

    Check links in MD files

--- generate-all.sh
+++ generate-all.sh
@@ -8,3 +8,8 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 
 $SCRIPT_DIR/doc/generate-doc.sh
+
+echo "checking links in MD files"
+./tools/md_check_links.py -d $SCRIPT_DIR -i ".*venv.*;.*site-packages.*"
+
+echo "generation completed"
