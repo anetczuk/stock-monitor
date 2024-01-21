18d8727a805e2e9171a5dba759aaf67e4dc90379
anetczuk <anetczuk@o2.pl>
2024-01-22 00:42:38 +0100

    Add description to output

--- src/transactioninfo.py
+++ src/transactioninfo.py
@@ -33,6 +33,7 @@ import pandas
 from stockmonitor.dataaccess.transactionsloader import load_mb_transactions
 from stockmonitor.datatypes.datacontainer import DataContainer
 from stockmonitor.datatypes.datatypes import TransactionMatchMode
+from pandas.core.frame import DataFrame
 
 # from dateutil.rrule import rrule, DAILY
 #
@@ -63,33 +64,98 @@ def load_dataframe( trans_hist_path ):
     return imported_data
 
 
-def store_dataframe( trans_data, out_trans_path ):
+def store_dataframe( trans_data: DataFrame, out_trans_path, description="" ):
     if not out_trans_path:
         return
-    if out_trans_path.endswith('.json'):
-        trans_data.to_json( out_trans_path, orient="records" )
-    elif out_trans_path.endswith('.xls'):
-        trans_data.to_excel( out_trans_path )
-    elif out_trans_path.endswith('.xlsx'):
-        # trans_data.to_excel( out_trans_path )
 
-        sheet_name = "data"
-        writer = pandas.ExcelWriter(out_trans_path, engine='xlsxwriter')        # pylint: disable=abstract-class-instantiated
-        trans_data.to_excel(writer, sheet_name=sheet_name, index=False)         # send df to writer
-        worksheet = writer.sheets[sheet_name]                                   # pull worksheet object
-        for idx, col in enumerate(trans_data):                                  # loop through all columns
-            series = trans_data[col]
-            max_len = max(( series.astype(str).map(len).max(),  # len of largest item
-                            len(str(series.name))               # len of column name/header
-                            )
-                          ) + 1  # adding a little extra space
-            worksheet.set_column(idx, idx, max_len)  # set column width
-        writer.save()
+    numpy_data_list = []
 
-    #elif out_trans_path.endswith('.csv'):
-    else:
-        trans_data.to_csv( out_trans_path, sep=';', encoding='utf-8' )
+    if description:
+        desc_dict = {"description": [description]}
+        description_frame = DataFrame.from_dict(desc_dict)
+        numpy_data_list.append(description_frame)
+
+    numpy_data_list.append(trans_data)
+    store_dataframe_list(out_trans_path, numpy_data_list)
+
     _LOGGER.info( "transactions stored to file %s", out_trans_path )
+
+
+def store_dataframe_list(out_file_path, numpy_data_list, horizontal=True):
+    if out_file_path.endswith('.json'):
+        with open(out_file_path, "w", encoding="utf-8") as data_file:
+            # clear file content
+            pass
+
+        for numpy_data in numpy_data_list:
+            with open(out_file_path, "a", encoding="utf-8") as data_file:
+                numpy_data.to_json(data_file, orient="records")
+
+    elif out_file_path.endswith('.csv'):
+        with open(out_file_path, "w", encoding="utf-8") as data_file:
+            # clear file content
+            pass
+
+        for numpy_data in numpy_data_list:
+            # horizontal not supported
+            with open(out_file_path, "a", encoding="utf-8") as data_file:
+                numpy_data.to_csv(data_file, sep=";", encoding="utf-8")
+            with open(out_file_path, "a", encoding="utf-8") as data_file:
+                data_file.write(";\n;\n")
+
+    elif out_file_path.endswith('.xls'):
+        sheet_name = "data"
+        # pylint: disable=abstract-class-instantiated
+        with pandas.ExcelWriter(out_file_path, engine="openpyxl") as writer:
+            start_row = 0
+            start_col = 0
+            for numpy_data in numpy_data_list:
+                curr_row = 0
+                curr_col = 0
+                if horizontal:
+                    curr_col = start_col
+                else:
+                    curr_row = start_row
+                numpy_data.to_excel(writer, sheet_name=sheet_name, index=False,
+                                    startrow=curr_row, startcol=curr_col)     # send df to writer
+                start_row += numpy_data.shape[0] + 3
+                start_col += numpy_data.shape[1] + 1
+
+    elif out_file_path.endswith('.xlsx'):
+        sheet_name = "data"
+        # pylint: disable=abstract-class-instantiated
+        with pandas.ExcelWriter(out_file_path, engine="xlsxwriter") as writer:
+            col_width_dict = {}
+            start_row = 0
+            start_col = 0
+            for numpy_data in numpy_data_list:
+                curr_row = 0
+                curr_col = 0
+                if horizontal:
+                    curr_col = start_col
+                else:
+                    curr_row = start_row
+                numpy_data.to_excel(writer, sheet_name=sheet_name, index=False,
+                                    startrow=curr_row, startcol=curr_col)  # send df to writer
+                start_row += numpy_data.shape[0] + 3
+                start_col += numpy_data.shape[1] + 1
+
+                # resize columns to fit content
+                for idx, col in enumerate(numpy_data):  # loop through all columns
+                    series = numpy_data[col]
+                    item_len = series.astype(str).map(len).max()  # len of largest item
+                    name_len = len(str(series.name))  # len of column name/header
+                    max_len = max(item_len, name_len) + 1  # adding a little extra space
+                    curr_width = col_width_dict.get(curr_col + idx, 0)
+                    col_width_dict[curr_col + idx] = max(max_len, curr_width)
+
+            worksheet = writer.sheets[sheet_name]  # pull worksheet object
+            for idx, width in col_width_dict.items():  # loop through all columns
+                worksheet.set_column(idx, idx, width)  # set column width
+
+    # else csv
+    else:
+        raise RuntimeError(f"unknown store format: {out_file_path}")
 
 
 def import_data( trans_hist_path ):
@@ -115,10 +181,10 @@ def extract_buysell( args ):
     if data_container is None:
         return False
 
-    trans_data = data_container.getWalletSellTransactions()
+    trans_data: DataFrame = data_container.getWalletSellTransactions()
     _LOGGER.info( "transactions:\n%s", trans_data )
 
-    store_dataframe( trans_data, out_trans_path )
+    store_dataframe( trans_data, out_trans_path, description="matched buy and sell transactions (FIFO match)" )
     return True
 
 
@@ -133,10 +199,10 @@ def extract_current( args ):
     if data_container is None:
         return False
 
-    trans_data = data_container.getWalletStock(show_soldout=True)
+    trans_data: DataFrame = data_container.getWalletStock(show_soldout=True)
     _LOGGER.info( "transactions:\n%s", trans_data )
 
-    store_dataframe( trans_data, out_trans_path )
+    store_dataframe( trans_data, out_trans_path, description="summary of current amounts of stock with sold-out stocks" )
     return True
 
 
@@ -151,10 +217,10 @@ def extract_currentbuy( args ):
     if data_container is None:
         return False
 
-    trans_data = data_container.getWalletBuyTransactions(sort_data=True)
+    trans_data: DataFrame = data_container.getWalletBuyTransactions(sort_data=True)
     _LOGGER.info( "transactions:\n%s", trans_data )
 
-    store_dataframe( trans_data, out_trans_path )
+    store_dataframe( trans_data, out_trans_path, description="list of current buy transactions (not sold)" )
     return True
 
 
@@ -171,10 +237,10 @@ def extract_walletvaluehistory( args ):
 
     _LOGGER.info( "preparing wallet value history" )
     ticker_list = None
-    values_data = data_container.getWalletValueHistory( "MAX", ticker_list )
+    values_data: DataFrame = data_container.getWalletValueHistory( "MAX", ticker_list )
     _LOGGER.info( "values:\n%s", values_data )
 
-    store_dataframe( values_data, out_path )
+    store_dataframe( values_data, out_path, description="history of value (capitalization) of wallet" )
     return True
 
 
@@ -190,10 +256,10 @@ def extract_walletgainhistory( args ):
         return False
 
     _LOGGER.info( "preparing wallet gain history" )
-    values_data = data_container.getWalletGainHistory( "MAX" )
+    values_data: DataFrame = data_container.getWalletGainHistory( "MAX" )
     _LOGGER.info( "values:\n%s", values_data )
 
-    store_dataframe( values_data, out_path )
+    store_dataframe( values_data, out_path, description="history of gain (saldo of sell transactions)" )
     return True
 
 
@@ -211,10 +277,15 @@ def extract_walletprofithistory( args ):
 
     _LOGGER.info( "preparing wallet profit history" )
     ticker_list = None
-    values_data = data_container.getWalletProfitHistory( "MAX", calculateOverall=calc_overall, tickerList=ticker_list )
+    values_data: DataFrame = data_container.getWalletProfitHistory( "MAX", calculateOverall=calc_overall,
+                                                                    tickerList=ticker_list )
     _LOGGER.info( "values:\n%s", values_data )
 
-    store_dataframe( values_data, out_path )
+    if calc_overall:
+        description = "history of overall profit (saldo of sells and capitalization of wallet"
+    else:
+        description = "history of saldo of wallet"
+    store_dataframe( values_data, out_path, description=description )
     return True
 
 
